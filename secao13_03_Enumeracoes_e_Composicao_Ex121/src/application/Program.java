8c07a59e8c8cb098b6eea80760c29c3438e7a26f
Daniel-Peres <dsperes@gmail.com>
2021-07-21 16:08:55 -0300

    secao13_03_Enumeracoes_e_Composicao_Ex121_falta apenas Program

--- secao13_03_Enumeracoes_e_Composicao_Ex121/src/application/Program.java
+++ secao13_03_Enumeracoes_e_Composicao_Ex121/src/application/Program.java
@@ -1,9 +1,33 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter cliente data:");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Email: ");
+		String email = sc.nextLine();
+		System.out.print("Birth date (DD/MM/YYYY): ");
+		String birthDate = sc.next();
+		System.out.print("Enter order data:");
+		
+		System.out.print("How many items to this order? ");
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
 
 	}
 
