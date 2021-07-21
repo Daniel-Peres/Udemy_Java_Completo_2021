9194bb1de345bfd9f465109745c913d3822cd078
Daniel-Peres <dsperes@gmail.com>
2021-07-21 16:43:25 -0300

    secao13_03_Enumeracoes_e_Composicao_Ex121_falta apenas Program

--- secao13_03_Enumeracoes_e_Composicao_Ex121/src/application/Program.java
+++ secao13_03_Enumeracoes_e_Composicao_Ex121/src/application/Program.java
@@ -1,32 +1,43 @@
 package application;
 
+import java.text.SimpleDateFormat;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("Enter cliente data:");
 		System.out.print("Name: ");
-		String name = sc.nextLine();
+		String clientName = sc.nextLine();
 		System.out.print("Email: ");
-		String email = sc.nextLine();
+		String clientEmail = sc.nextLine();
 		System.out.print("Birth date (DD/MM/YYYY): ");
-		String birthDate = sc.next();
-		System.out.print("Enter order data:");
-		
+		String clientBirthDate = sc.next();
+		System.out.println("Enter order data:");
+		System.out.print("Status: ");
+		String status = sc.next();
+
 		System.out.print("How many items to this order? ");
-		
-		
-		
-		
-		
-		
-		
+		int n = sc.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			System.out.println("Enter #" + i + " item data:");
+			System.out.print("Product name: ");
+			String productName = sc.nextLine();
+			System.out.print("Product price: ");
+			double productPrice = sc.nextDouble();
+			System.out.print("Quantity: ");
+			int productQuantity = sc.nextInt();
+			
+
+		}
+
 		sc.close();
 
 	}
