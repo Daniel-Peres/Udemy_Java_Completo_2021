e0157b04c8997afac73f15f08b06ae46bff47897
Daniel-Peres <dsperes@gmail.com>
2021-08-10 15:25:08 -0300

    secao16_Projeto_Sistema_Jogo_de_Xadrez__camada_Chess_e_imprimir_tabuleiro_(comentado)

--- secao16_Projeto_Sistema_Jogo_de_Xadrez/src/application/UI.java
+++ secao16_Projeto_Sistema_Jogo_de_Xadrez/src/application/UI.java
@@ -4,20 +4,21 @@ import chess.ChessPiece;
 
 public class UI {
 
-	public static void printBoard(ChessPiece[][] pieces) {
+	public static void printBoard(ChessPiece[][] pieces) { // imprimir tabuleiro
 
 		for (int i = 0; i < pieces.length; i++) {
-			System.out.print((8 - i) + " ");
+			System.out.print((8 - i) + " "); // imprime numero lateral esquerdo com espaço
 			for (int j = 0; j < pieces.length; j++) {
-				printPiece(pieces[i][j]);
+				printPiece(pieces[i][j]); // imprime pecas dispostas no tabuleiro
 			}
-			System.out.println();
+			System.out.println(); // quebra de linha
 		}
-		System.out.println("  a b c d e f g h");
+		System.out.println("  a b c d e f g h"); // imprime rodapé do tabuleiro
 
 	}
 
-	private static void printPiece(ChessPiece piece) {
+	private static void printPiece(ChessPiece piece) { // imprimir uma unica peca
+
 		if (piece == null) { // se peca nula imprime traço
 			System.out.print("-");
 		} else { // senão imprime peça
--- secao16_Projeto_Sistema_Jogo_de_Xadrez/src/chess/ChessMatch.java
+++ secao16_Projeto_Sistema_Jogo_de_Xadrez/src/chess/ChessMatch.java
@@ -6,11 +6,11 @@ public class ChessMatch {
 
 	private Board board;
 
-	public ChessMatch() {
+	public ChessMatch() { // a classe partida de xadrez instancia um tabuleiro com seu tamanho em seu construtor
 		board = new Board(8, 8);
 	}
 
-	public ChessPiece[][] getPieces() {
+	public ChessPiece[][] getPieces() {  // fazendo downCast nas do tipo peça pra peças de xadrez
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
 		for (int i = 0; i < board.getRows(); i++) {
 			for (int j = 0; j < board.getColumns(); j++) {
@@ -18,7 +18,7 @@ public class ChessMatch {
 			}
 		}
 
-		return mat;
+		return mat; //retornando a matriz de peças agora com o tipo ChessPiece
 	}
 
 }
