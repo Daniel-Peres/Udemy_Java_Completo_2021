b706344a3968b9b0f6e253dd83ecff2b4b20017b
Daniel-Peres <dsperes@gmail.com>
2021-09-08 13:31:23 -0300

    secao17_Trabalhando_com_Arquivos_Classes_FileWriter_e_BufferedWriter

---
+++ secao17_04_Trabalhando_com_arquivos_FileWriter_e_BufferedWriter/src/application/Program.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		String[] lines = new String[] { "Good morning", "Good afternoon", "Good night" };
+
+		String path = "C:\\eclipse_workspace\\Udemy_Java_Completo_2021\\secao17_01_Trabalhando_com_arquivos_File_e_Scanner\\out.txt";
+
+		// com essa linha do try reescreve o arquivo
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+		// com a linha do try abaixo, o conteudo é acrescentado no arquivo original
+		//try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {			
+			for(String line : lines) {
+				bw.write(line);
+				bw.newLine();
+			}
+
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
